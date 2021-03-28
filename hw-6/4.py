d3989773a395156ca19765ea03dd06fe9822c614
shutkos <shutkos@gmail.com>
2021-03-28 23:40:19 +0300

    init

--- hw-6/4.py
+++ hw-6/4.py
@@ -1,19 +1,9 @@
-# Реалізуйте клас Mathematician, який буде допоміжним класом для проведення математичних операцій над списком цілих
-# чисел. Клас не приймає жодних атрибутів і має тільки методи:
-# (1) square_nums (приймає список цілих чисел і повертає список їхніх квадратів);
-# (2) remove_positives (приймає список цілих чисел і повертає його без додатних чисел);
-# (3) filter_leaps (приймає список років (цілих чисел) і прибирає невисокосні).
-# Ці методи мають бути не визначені безпосередньо в цьому класі, а отримані за допомогою наслідування з трьох базових
-# класів із цими методами. Іншими словами, потрібно створити три окремі класи, які інкапсулюють описану логіку, але
-# клас Mathematician потримує її через наслідування. Також потрібно створити окремий клас, який валідує параметр,
-# переданий у методи. Має викликатися TypeError, якщо параметр не є списком, і ValueError, якщо переданий список не
-# є списком цілих чисел.
 class ValidationMixin:
 
     def validate(self, list_int):
         if not isinstance(list_int, list):
             raise TypeError
-        elif not isinstance(list_int, int):
+        elif not all(isinstance(x, int) for x in list_int):
             raise ValueError
         else:
             return True
@@ -44,7 +34,8 @@ class FilterLeaps(ValidationMixin):
         if self.validate(list_int):
             data = []
             for i in list_int:
-                if (i % 4 == 0) and (i % 100 != 0) or (i % 400 == 0):
+                if (i % 4 == 0) and (i % 100 != 0) or (i % 400 == 0) and (i > 0):
+                    print(i)
                     data.append(i)
             return data
 
