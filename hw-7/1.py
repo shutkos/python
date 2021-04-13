d68b37044c68c5ed7dae69e4a2aaeafef50e5bf9
shutkos <shutkos@gmail.com>
2021-04-13 21:05:34 +0300

    init

---
+++ hw-7/1.py
@@ -0,0 +1,28 @@
+# Створіть базовий клас Animal із методом talk. Потім створіть два підкласи: Dog і Cat з їхніми власними реалізаціями
+# методу talk. Наприклад, у Dog може бути print 'woof woof', а у Cat – print 'meow'. Також створіть просту функцію, яка
+# приймає на вхід екземпляр класу Cat або Dog і виконує метод talk для вхідного параметра.
+
+class Animal:
+
+    def talk(self):
+        pass
+
+
+class Dog(Animal):
+
+    def talk(self):
+        print('woof woof')
+
+
+class Cat(Animal):
+
+    def talk(self):
+        print('meow')
+
+
+def simple(animal):
+    animal.talk()
+
+
+simple(Cat())
+simple(Dog())
